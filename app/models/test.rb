75180ce8b9f0d85c5bca531af23292fa70a0310b
Елена <drawing-pin91@mail.ru>
2022-08-01 19:20:57 +0300

    create seeeds and methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.for_category(name)
+    categories = Category.where(title: name)
+    tests = Test.find_by(category_id: categories)
+   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def test_level(num)
+    Test.where(level: num)
+  end
 end
