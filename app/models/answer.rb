9dcdc4207c325f655e1b71336dd69d8e4a9bfe1e
Елена <drawing-pin91@mail.ru>
2022-08-12 21:39:10 +0300

    fix

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,15 @@ class Answer < ApplicationRecord
   belongs_to :question
   
   validates :body, presence: true
+  validate :max_count
   
   scope :correct, -> { where(correct: true) }
+
+  private
+  
+  def max_count
+    if self.class.where(question_id: question_id).size >= 4
+      errors.add(:max_count) 
+    end
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,13 +3,4 @@ class Question < ApplicationRecord
   belongs_to :test
 
   validates :body, presence: true
-  validate :answer_count
-
-  privete
-  
-  def answer_count
-    if answers.size.zero? || answers.size > 4
-      errors.add(:answers) 
-    end
-  end
 end
