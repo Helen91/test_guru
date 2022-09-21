0ef3bd6d0e240573a5967a2d05581b21c0577707
Елена <drawing-pin91@mail.ru>
2022-09-21 16:23:22 +0300

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_15_080659) do
+ActiveRecord::Schema.define(version: 2022_09_21_131910) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2022_09_15_080659) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
