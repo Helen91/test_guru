0bbbd9588c483d1624062b830c80a267b7b050b2
Елена <drawing-pin91@mail.ru>
2022-10-04 17:06:11 +0300

    add local

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :set_locael
+
   protected
 
   def configure_permitted_parameters
@@ -14,4 +16,15 @@ class ApplicationController < ActionController::Base
       root_url
     end
   end
+
+  def default_url_options
+    {lang: I18n.locale}
+  end
+
+  private
+
+  def set_locael
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
+
 end
