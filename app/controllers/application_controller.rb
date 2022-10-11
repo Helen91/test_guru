009bed67847e62b8c601df817eb1a2b2745189ca
Елена <drawing-pin91@mail.ru>
2022-10-11 15:06:24 +0300

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,11 +18,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    if I18n.default_locale == I18n.locale
-      {}
-    else 
-      {lang: I18n.locale}
-    end
+    I18n.default_locale == I18n.locale ? {} : {lang: I18n.locale}
   end
 
   private
